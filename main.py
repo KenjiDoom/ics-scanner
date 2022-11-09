8b3f10218494c661723f0422c6f247c33b029603
KenjiDoom <77996865+KenjiDoom@users.noreply.github.com>
2022-11-08 19:43:47 -0800

    Setting up
    Showing available scada systems that we can scan

--- main.py
+++ main.py
@@ -1,20 +1,40 @@
 from rich.console import Console
 from rich.table import Table
 from sys import exit
-import shodan 
-import nmap3
+import shodan
 
 class scanner:
+    console = Console()
     def __init__(self, *argv):
         for argv in argv:
             self.option = argv
             print("Working " + self.option)
 
     def nmap_scanning(self):
-        pass
+        console.print("[bold green] Starting nmap scan on this device")
 
     def output_data(self):
         pass
 
-console = Console()
-SCAN = scanner(console.input("[bold red]Enter the ICS product you want to scan: "))
+
+
+class startProgram:
+    def run_script(self):
+        console  = Console()    
+        scada_selection()
+        SCAN = scanner(console.input("[bold red]Enter the ICS product you want to scan: "))
+
+    def scada_selection(self):
+        console  = Console()
+        """
+Going a bit old school with this apporach, please don't bash me. Possible UI in the future.
+Huge thanks to digital bond for providing these scripts.
+        """
+        console.print("[bold blue][-] AVAILABLE SCADA SCRIPTS [-]")
+        console.print("[bold white][1] Rockwell Automation, port 44818")
+        console.print("[bold white][2]. Niagara Fox, Port 1911")
+        console.print("[bold white][3]. ATG, Port 10001")
+
+
+start = startProgram()
+start.scada_selection()
