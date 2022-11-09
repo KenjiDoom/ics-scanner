79b24f428a6aa3c9cd49a43a07c5c469077eff04
KenjiDoom <77996865+KenjiDoom@users.noreply.github.com>
2022-11-08 19:44:08 -0800

    Update main.py

--- main.py
+++ main.py
@@ -8,20 +8,23 @@ class scanner:
     def __init__(self, *argv):
         for argv in argv:
             self.option = argv
-            print("Working " + self.option)
 
-    def nmap_scanning(self):
-        console.print("[bold green] Starting nmap scan on this device")
+    def rockwell_automation(self):
+        print("You chose rockwell")
+    
+    def niagara_fox(self):
+        print("You chose niagara")
+    
+    def atg(self):
+        print("You chose atg")
 
     def output_data(self):
         pass
 
-
-
 class startProgram:
-    def run_script(self):
-        console  = Console()    
-        scada_selection()
+    def run_program(self):
+        console = Console()
+        self.scada_selection()
         SCAN = scanner(console.input("[bold red]Enter the ICS product you want to scan: "))
 
     def scada_selection(self):
@@ -31,10 +34,10 @@ Going a bit old school with this apporach, please don't bash me. Possible UI in 
 Huge thanks to digital bond for providing these scripts.
         """
         console.print("[bold blue][-] AVAILABLE SCADA SCRIPTS [-]")
-        console.print("[bold white][1] Rockwell Automation, port 44818")
+        console.print("[bold white][1]. Rockwell Automation, port 44818")
         console.print("[bold white][2]. Niagara Fox, Port 1911")
         console.print("[bold white][3]. ATG, Port 10001")
 
 
 start = startProgram()
-start.scada_selection()
+start.run_program()
