d150656dd67ab8af870b78ee33c2ee05ea983cab
KenjiDoom <77996865+KenjiDoom@users.noreply.github.com>
2022-11-29 13:59:51 -0800

    Shodan Search Scan
    Shodan API search results for a given IP code was added. This is the same code as Beautiful-Sho

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from rich.table import Table
 from rich.panel import Panel
 import subprocess
 import getpass
+import shodan
 
 class scanner:
     console = Console()
@@ -17,6 +18,9 @@ class scanner:
             self.rockwell_automation()
         elif self.option == '2':
             self.niagara_fox()
+        elif self.option == '3':
+            self.shodan_scan()
+            self.output_format()
         else:
             self.console.print(self.option, "is not an option, exiting...")
             exit()
@@ -32,7 +36,7 @@ class scanner:
             subprocess.run(['clear'])
             self.console.log(table.add_row(subprocess.Popen(['nmap', '--script', 'enip-info', '-Pn', '-p', '44818', self.IP, '-oN', self.IP + '_TCP_SCAN.txt'], stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()[0].decode('utf-8'), subprocess.Popen(['nmap', '--script', 'enip-info', '-sU', '-Pn', '-p', '44818', self.IP, '-oN', self.IP + '_UDP_SCAN.txt'], stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()[0].decode('utf-8')))
         self.console.print(table)
-    
+
     def niagara_fox(self):
         table = Table(title="NMAP RESULTS")
         table.add_column("Nmap results for Niagara Fox System")
@@ -41,10 +45,34 @@ class scanner:
             table.add_row(subprocess.Popen(['nmap', '-Pn', '-sT', '--script', 'fox-info.nse', '-p', '1911,4911', self.IP, '-oN', self.IP + '_TCP.txt'], stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()[0].decode('utf-8'))
         self.console.print(table)
 
-    def atg(self):
-        pass
-        # To-do: Fix the orginal nse script, doesn't work becasue the code version is deprecated
-        # Also note I tried using the nmap python API but it doesn't provide custom scripts
+    def shodan_scan(self):
+        api = shodan.Shodan()
+        host = api.host(self.IP)
+        self.ip, self.banner, self.port, self.city, self.domains, self.asn = [],[],[],[],[],[]
+        for items in host['data']:
+            self.ip.append(items['ip_str'])
+            self.banner.append(items['data'])
+            self.port.append(items['port'])
+            self.city.append(host.get('city', 'n/a'))
+            self.domains.append(host.get('domain', 'n/a'))
+            self.asn.append(items['asn'])
+
+        #print(len(self.ip), len(self.city), len(self.port), len(self.domains), len(self.asn)) # Verifying that they all match
+    def output_format(self):
+        table = Table(title="Shodan results")
+        table = Table(show_lines = True)
+
+        table.add_column("IP", justify="right", style="green", no_wrap=True)
+        table.add_column("Banner", justify="right", style="white")
+        table.add_column("Port", style="red")
+        table.add_column("City", justify="right", style="green")
+        table.add_column("Domains", style="red")
+        table.add_column("ASN", style="Cyan")
+        
+        for index, (a, b, c, d, e, f) in enumerate(zip(self.ip, self.banner, self.port, self.city, self.domains, self.asn)):
+            table.add_row(str(a), str(b), str(c), str(d), str(e), str(f))
+        
+        self.console.print(table)
 
 class startProgram:
     console = Console()
@@ -63,8 +91,9 @@ class startProgram:
     def scada_menu(self):
         table = Table(title="[bold red] Scada Scans available ", title_style="bold magenta")
         table.add_column(header="1.", justify="left", no_wrap=True)
-        table.add_column(header="2.", justify="right", no_wrap=True)
-        table.add_row(Panel.fit("Rockwell Automation port 44818"), Panel.fit("Niagara Fox port 1911 or 4199"))
+        table.add_column(header="2.", justify="middle", no_wrap=True)
+        table.add_column(header="3.", justify="right", no_wrap=True)
+        table.add_row(Panel.fit("Rockwell Automation port 44818"), Panel.fit("Niagara Fox port 1911 or 4199"), Panel.fit("Shodan Scan Results"))
         self.console.print(table)
     
     def IP_menu(self):
@@ -93,6 +122,7 @@ class startProgram:
                 self.console.print("\nExiting...")
                 exit()
 
-start = startProgram()
-start.get_root()
-start.run_program()
+if __name__ == '__main__':
+    start = startProgram()
+    start.get_root()
+    start.run_program()
